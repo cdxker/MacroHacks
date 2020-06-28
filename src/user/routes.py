ac8cfa352614d119d4e0c3b60d33a13214d8191a
Denzell Ford <fdenzell@gmail.com>
2020-06-27 19:42:27 -0500

    added conversations

--- src/user/routes.py
+++ src/user/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, url_for, abort, redirect, flash
+from flask import Blueprint, render_template, request, url_for, abort, redirect, flash, jsonify
 import random
 from src.ext import bcrypt, db
-from .schemas import UserSchema
-from .models import User
+from .schemas import UserSchema, LoginSchema, MessageSchema
+from .models import User, Message, Conversation
 from slugify import slugify
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -11,52 +11,71 @@ user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/register', methods=["GET", "POST"])
 def register():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('forum.main'))
-    # form = RegistrationForm()
-    # if form.validate_on_submit():
-    #     hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-    #     default_images = [f"default{i}.jpg" for i in range(1, 5)]
-    #     user = User(username=form.username.data, email=form.email.data, password=hashed_password, image_file=random.choice(default_images))
-    #     db.session.add(user)
-    #     db.session.commit()
-    #     flash('Your account has been created you are now able to login', 'success')
-    #     redirect(url_for("forum.login"))
-    return render_template('register.html')
+
+    user = UserSchema.load(request.form)
+    user.password = bcrypt.generate_password_hash(user.password).decode('utf-8')
+    user.slug = slugify(user.username)
+    
+    if User.query.filter_by(slug=user.slug).first():
+        return jsonify({"status" : "Error", "msg" : "Username already exists"})
+
+    if User.query.filter_by(email=user.email).first():
+        return jsonify({"status" : "Error", "msg" : "Email already exists"})
+
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify({ "status" : "Success" })
 
 @user_bp.route("/login", methods=["GET", "POST"])
 def login():
-    # schema = UserSchema()
-    # form = schema.load(request.form)
 
-    # if current_user.is_authenticated:
-    #     return redirect(url_for("user.home"))
-
-    # if request.method == "POST":
-
-    #     user = User.query.filter_by(username=form["username"]).first()
-    #     if user and bcrypt.check_password_hash(user.password, form.password.data):
-    #         login_user(user, remember=True)
-    #         flash("You are now logged in", "success")
-    #         next_page = request.args.get('next')
-    #         return redirect(next_page) if next_page else redirect(url_for("home.home"))
-    #     else: 
-    #         flash("Wrong username or password", "warning")
-
+    if request.method == "POST":
+        schema = LoginSchema()
+        body = schema.load(request.form)
+        
+        user = User.query.filter_by(username=body["username"]).first()
+        if user and bcrypt.check_password_hash(user.password, body["password"]):
+            login_user(user, remember=form.remember.data)
+            flash("You are now logged in", "success")
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for("home.home"))
+        return jsonify({ "status": "Error", "msg": "Wrong username or password" })
 
     return render_template('login.html')
 
 
 @user_bp.route("/home")
+@login_required
 def home():
 
     return render_template("home.html")
 
 @user_bp.route("/all_chats")
+@login_required
 def messages():
     return render_template("all_chats.html")
 
-@user_bp.route("/chat/")
+@user_bp.route("/chat")
+@login_required
 def chat():
     return render_template("chat.html")
+
+############### JSON API STUFF ###############
+
+@user_bp.route("/send_message", methods=["POST"])
+@login_required
+def send_message():
+
+    message = MessageSchema().load(request.json)
+    message.user_id = current_user.id
+    db.session.add(message)
+    db.session.commit()
+
+@user_bp.route("/messages/<convo_id>")
+def get_messages(convo_id):
+
+    convo = Conversation.query.get_or_404(convo_id)
+
+    return jsonify(MessageSchema(many=True).dump(convo.messages))
 
--- src/user/schemas.py
+++ src/user/schemas.py
@@ -1,9 +1,21 @@
 from src.ext import ma
-from .models import User
+from .models import User, Message
 from marshmallow import fields, Schema
+
+class LoginSchema(Schema):
+
+    username = fields.String()
+    password = fields.String()
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
 
     class Meta:
         model = User
+        load_instance = True
 
+class MessageSchema(ma.SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Message
+        load_instance = True
+    
